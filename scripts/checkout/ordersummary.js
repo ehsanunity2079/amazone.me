ccf4ee632747ac134d51398ad832dc9463e07da8
ehsanunity2079 <ehsanunity2079@gmail.com>
2024-12-30 13:09:39 +0330

    Make payment summary interactiv

--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -4,12 +4,7 @@ import { formatCurrency } from '../Utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -121,6 +116,9 @@ export function renderOrderSummary() {
           `.js-cart-item-container-${productId}`
         );
         container.remove();
+
+
+        renderPaymentSummary();
       });
     });
 
@@ -131,6 +129,7 @@ export function renderOrderSummary() {
           const {productId,deliveryOptionId}=element.dataset
           updateDeliveryOption(productId, deliveryOptionId);
           renderOrderSummary();
+          renderPaymentSummary();
         });
       });
  }
