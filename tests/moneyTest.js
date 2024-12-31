d35d63c257cf3443339813737e8da266259be83b
ehsanunity2079 <ehsanunity2079@gmail.com>
2024-12-31 18:15:09 +0330

    Remove default tests and add money tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -2,7 +2,6 @@ import {formatCurrency} from '../scripts/Utils/money.js';
 
 console.log('converts cents into dollars');
 
-
 if (formatCurrency(2095) === '20.95') {
   console.log('passed');
 } else {
